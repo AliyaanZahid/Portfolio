8f136eb8fa954d027562db937640b99365e03a9e
Aliyaan Zahid <aliyaanzahid21@gmail.com>
2025-03-09 21:42:23 -0400

    Updated features and bug fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -195,19 +195,30 @@ document.onkeydown = function (e) {
     }
 }
 
-/*
-// Start of Tawk.to Live Chat
+
+/*// Start of Tawk.to Live Chat
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/65d41b1f9131ed19d96ed199/1hn28jsje';
+s1.src='https://embed.tawk.to/67ce3e5037c56d190f3f1f8e/1iluq6qoc';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();*/
+
+//<!--Start of Tawk.to Script-->
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/67ce3e5037c56d190f3f1f8e/1iluq6qoc';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
+//<!--End of Tawk.to Script-->
 
-// <!--End of Tawk.to Script--> */
 
 
 
